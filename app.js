109c7bab405f473d5840fad53aa1d0bc225ced1b
rahulfordev <rahulforsms@gmail.com>
2024-09-30 15:08:20 +0600

    code update

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 // Import routers
-const userRouter = require("./routes/users");
-const bookRouter = require("./routes/book");
+const userV1Router = require("./routes/v1/users");
+const bookV1Router = require("./routes/v1/book");
 
 // Middleware
 app.use(express.json());
 
 // Use routers
-app.use("/user", userRouter);
-app.use("/book", bookRouter);
+app.use("/api/v1/user", userV1Router);
+app.use("/api/v1/book", bookV1Router);
 
 module.exports = app;
